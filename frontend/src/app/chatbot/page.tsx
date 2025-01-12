05210e6a7e8a045555a67477dd6e5aef8a1f957a
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-01-12 08:47:04 +0545

    feat: implement basic qna bot

--- frontend/src/app/chatbot/page.tsx
+++ frontend/src/app/chatbot/page.tsx
@@ -5,130 +5,135 @@ import { Trash2 } from "lucide-react";
 import React from "react";
 
 interface Message {
-  content: string;
-  sender: "user" | "bot";
-  timestamp: Date;
+	content: string;
+	sender: "user" | "bot";
+	timestamp: Date;
 }
 
 export default function ChatBot() {
-  const [messages, setMessages] = React.useState<Message[]>([]); // State to hold chat messages
-  const [newMessage, setNewMessage] = React.useState("");
-  const messagesEndRef = React.useRef<HTMLDivElement>(null);
+	const [messages, setMessages] = React.useState<Message[]>([]); // State to hold chat messages
+	const [newMessage, setNewMessage] = React.useState("");
+	const messagesEndRef = React.useRef<HTMLDivElement>(null);
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
+	const scrollToBottom = () => {
+		messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+	};
 
-  React.useEffect(() => {
-    if (messages.length > 0) {
-      scrollToBottom();
-    }
-  }, [messages]);
+	React.useEffect(() => {
+		if (messages.length > 0) {
+			scrollToBottom();
+		}
+	}, [messages]);
 
-  const handleAddMessage = async (message: string) => {
-    if (!message.trim()) return;
+	const handleAddMessage = async (message: string) => {
+		if (!message.trim()) return;
 
-    const userMessage: Message = {
-      content: message,
-      sender: "user",
-      timestamp: new Date(),
-    };
+		const userMessage: Message = {
+			content: message,
+			sender: "user",
+			timestamp: new Date(),
+		};
 
-    setMessages((prevMessages) => [...prevMessages, userMessage]);
+		setMessages((prevMessages) => [...prevMessages, userMessage]);
 
-    try {
-      const botResponse: Message = {
-        content: `${message}`, // Simulated bot response
-        sender: "bot",
-        timestamp: new Date(),
-      };
+		try {
+			const botResponse: Message = {
+				content: `${message}`, // Simulated bot response
+				sender: "bot",
+				timestamp: new Date(),
+			};
 
-      setMessages((prevMessages) => [...prevMessages, botResponse]);
-    } catch (error) {
-      console.error("Failed to send message:", error);
-    }
+			const res = await fetch(
+				"http://localhost:8002/lawyer?question=" +
+				message +
+				"&collection_name=test8",
+			);
+			const data = await res.json();
 
-    setNewMessage("");
-  };
+			if (data.answer) {
+				botResponse.content = data.answer;
+			}
 
-  const handleDeleteChat = () => {
-    setMessages([]);
-  };
+			setMessages((prevMessages) => [...prevMessages, botResponse]);
+		} catch (error) {
+			console.error("Failed to send message:", error);
+		}
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    handleAddMessage(newMessage);
-  };
+		setNewMessage("");
+	};
 
-  return (
-    <div className="container mx-auto h-screen flex flex-col justify-between">
-      {/* Chat Header */}
-      <div className="flex flex-row justify-between items-center w-full rounded-t-lg border-b p-4">
-        <h2 className="font-bitter text-2xl font-semibold">AI Lawyer</h2>
-        <Button
-          className="text-dark-gray hover:text-dark-red"
-          onClick={handleDeleteChat}
-        >
-          <Trash2 size={24} />
-        </Button>
-      </div>
+	const handleDeleteChat = () => {
+		setMessages([]);
+	};
 
-      {/* Chat Messages */}
-      <div
-        className="flex-1 overflow-y-auto p-4 space-y-4 w-full max-w-3xl"
-        style={{ maxHeight: "calc(100vh - 160px)" }} // Adjusts the height based on header and footer
-      >
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`flex ${
-              message.sender === "bot" ? "justify-start" : "justify-end"
-            }`}
-          >
-            <div
-              className={`max-w-[70%] rounded-lg p-3 ${
-                message.sender === "bot"
-                  ? "bg-light-gray text-dark-gray mr-4"
-                  : "bg-light-blue text-white ml-4"
-              }`}
-            >
-              <p className="whitespace-pre-wrap">{message.content}</p>
-              <span className="text-xs opacity-70 block mt-1">
-                {message.timestamp.toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </span>
-            </div>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </div>
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		handleAddMessage(newMessage);
+	};
 
-      {/* Input Field */}
-      <form
-        onSubmit={handleSubmit}
-        className="p-4 border-t w-full rounded-b-lg"
-      >
-        <div className="flex space-x-2 h-full">
-          <input
-            type="text"
-            value={newMessage}
-            onChange={(e) => setNewMessage(e.target.value)}
-            placeholder="Type your message..."
-            className="flex-1 p-2 border text-primary-text-light dark:text-primary-text-dark rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <Button
-            className={`px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 
+	return (
+		<div className="container mx-auto h-screen flex flex-col justify-between">
+			{/* Chat Header */}
+			<div className="flex flex-row justify-between items-center w-full rounded-t-lg border-b p-4">
+				<h2 className="font-bitter text-2xl font-semibold">AI Lawyer</h2>
+				<Button
+					className="text-dark-gray hover:text-dark-red"
+					onClick={handleDeleteChat}
+				>
+					<Trash2 size={24} />
+				</Button>
+			</div>
+
+			<div className="mx-auto w-full max-w-3xl">
+				{/* Chat Messages */}
+				<div
+					className="flex-1 overflow-y-auto p-4 space-y-4 w-full"
+					style={{ maxHeight: "calc(100vh - 160px)" }} // Adjusts the height based on header and footer
+				>
+					{messages.map((message, index) => (
+						<div
+							key={index}
+							className={`flex ${message.sender === "bot" ? "justify-start" : "justify-end"
+								}`}
+						>
+							<div
+								className={`max-w-[70%] rounded-lg p-3 text-background ${message.sender === "bot"
+										? "bg-light-gray mr-4"
+										: "bg-light-blue ml-4"
+									}`}
+							>
+								<p className="whitespace-pre-wrap">{message.content}</p>
+							</div>
+						</div>
+					))}
+					<div ref={messagesEndRef} />
+				</div>
+
+				{/* Input Field */}
+				<form
+					onSubmit={handleSubmit}
+					className="p-4 border-t w-full rounded-b-lg"
+				>
+					<div className="flex space-x-2 h-full">
+						<input
+							type="text"
+							value={newMessage}
+							onChange={(e) => setNewMessage(e.target.value)}
+							placeholder="Type your message..."
+							className="flex-1 p-2 border text-background rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+						<Button
+							className={`px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 
               transition-colors flex items-center space-x-2
               disabled:bg-blue-300 disabled:cursor-not-allowed`}
-            type="submit"
-            disabled={!newMessage.trim()}
-          >
-            Send
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+							type="submit"
+							disabled={!newMessage.trim()}
+						>
+							Send
+						</Button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,34 +3,34 @@ import { Bitter, Noto_Sans } from "next/font/google";
 import "./globals.css";
 
 const noto = Noto_Sans({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-noto",
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-noto",
 });
 
 const bitter = Bitter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-bitter",
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-bitter",
 });
 
 export const metadata: Metadata = {
-  title: "NPI - checklist",
-  description: "Checklist for NPI",
+	title: "NPI - checklist",
+	description: "Checklist for NPI",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`font-noto ${noto.variable} ${bitter.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body
+				className={`font-noto ${noto.variable} ${bitter.variable} antialiased dark`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
