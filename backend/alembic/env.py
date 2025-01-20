01f429b62696e1e4823d786fe18ebf9a98f29cda
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-01-20 20:15:41 +0545

    feat: add init.sh and run.sh

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -7,7 +7,7 @@ from alembic import context
 
 from app.core.database import Base
 from app.models.user import User, metadata
-from app.models.lawyer import QueryResponse
+from app.models.lawyer import QueryResponse, UserQueryRelation
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
