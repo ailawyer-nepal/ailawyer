3a2261c886d60968e4b3516c9604b2ea4c285bb9
Abhiyan Dhakal <itsabhiyandhakal@gmail.com>
2025-06-18 07:01:54 +0545

    feat(web): update metadata and favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,34 +3,35 @@ import { Bitter, Noto_Sans } from "next/font/google";
 import "./globals.css";
 
 const noto = Noto_Sans({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-noto",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-noto",
 });
 
 const bitter = Bitter({
-	subsets: ["latin"],
-	display: "swap",
-	variable: "--font-bitter",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bitter",
 });
 
 export const metadata: Metadata = {
-	title: "NPI - checklist",
-	description: "Checklist for NPI",
+  title: "Nepal AI Legal Assistant",
+  description:
+    "This project explores the feasibility of artificial intelligence-driven analysis in the context of the Nepalese legal system, providing semantic understanding of legal queries with advanced AI technology.",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`font-noto ${noto.variable} ${bitter.variable} antialiased dark`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`font-noto ${noto.variable} ${bitter.variable} antialiased dark`}
+      >
+        {children}
+      </body>
+    </html>
+  );
 }
